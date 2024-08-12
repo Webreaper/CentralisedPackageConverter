eefaac29c72148782dcd7236522aa798f3584eed
Steven Liekens <steven.liekens@gmail.com>
2024-08-12 22:23:15 +0000

    Fix crash in Spectre console logging

--- CentralisedPackageConverter/OutputExtensions.cs
+++ CentralisedPackageConverter/OutputExtensions.cs
@@ -8,14 +8,14 @@ namespace CentralisedPackageConverter;
 public static class Output
 {
     public static void InfoLine(string message) => InfoLine(message, Array.Empty<object>());
-    public static void InfoLine(string message, params object[] args) => AnsiConsole.MarkupLine($"{message}", args);
+    public static void InfoLine(string message, params object[] args) => AnsiConsole.MarkupLine(Markup.Escape(message), args);
 
     public static void ErrorLine(string message) => ErrorLine(message, Array.Empty<object>());
-    public static void ErrorLine(string message, params object[] args) => AnsiConsole.MarkupLine($"[red]{message}[/]", args);
+    public static void ErrorLine(string message, params object[] args) => AnsiConsole.MarkupLineInterpolated($"[red]{string.Format(message, args)}[/]");
 
     public static void TraceLine(string message) => TraceLine(message, Array.Empty<object>());
-    public static void TraceLine(string message, params object[] args) => AnsiConsole.MarkupLine($"[grey]{message}[/]", args);
+    public static void TraceLine(string message, params object[] args) => AnsiConsole.MarkupLineInterpolated($"[grey]{string.Format(message, args)}[/]");
 
     public static void WarningLine(string message) => WarningLine(message, Array.Empty<object>());
-    public static void WarningLine(string message, params object[] args) => AnsiConsole.MarkupLine($"[yellow]{message}[/]", args);
+    public static void WarningLine(string message, params object[] args) => AnsiConsole.MarkupLineInterpolated($"[yellow]{string.Format(message, args)}[/]");
 }
